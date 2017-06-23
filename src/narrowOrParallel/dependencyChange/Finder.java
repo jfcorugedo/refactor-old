2cb2e8c64bc8ad41884e97d1ae24c612dda638e3
Juan Fernandez-Corugedo <jfcorugedo@gmail.com>
2017-06-23 09:25:36 +0200

    Inline between factory method and the constructor without parser

--- src/narrowOrParallel/dependencyChange/Finder.java
+++ src/narrowOrParallel/dependencyChange/Finder.java
@@ -7,17 +7,9 @@ public class Finder {
     UserRepository repository;
     Parser parser;
 
-    Finder(UserRepository repository) {
-        this.repository = repository;
-    }
-
     public Finder(UserRepository repository, Parser parser) {
         this.repository = repository;
         this.parser = parser;
-    }
-
-    public static Finder createFinder(UserRepository repository) {
-        return new Finder(repository, new Parser());
     }
 
     public List<User> find(String query){
